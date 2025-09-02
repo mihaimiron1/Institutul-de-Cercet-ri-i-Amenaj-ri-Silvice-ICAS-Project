8e55b7e5d221a36ba7c416fdc073fc49087ca943
mihaimiron1 <mihai.miron@isa.utm.md>
2025-09-02 17:10:08 +0300

    login page

--- backend/silva/settings.py
+++ backend/silva/settings.py
@@ -29,8 +29,10 @@ DEBUG = env("DEBUG")
 SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
-LOGIN_URL = '/accounts/login/'
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
+
 
 
 # Application definition
